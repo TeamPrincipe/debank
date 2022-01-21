10c7641df69a48abb3aa45da7d353b9e57e277b7
TeamPrincipe <94296220+TeamPrincipe@users.noreply.github.com>
2022-01-21 21:12:14 +0100

    nuovi esempi

--- example.py
+++ example.py
@@ -9,12 +9,14 @@ from debank.wallet import Wallet
 
 # EXAMPLE  
 address = "your_wallet_address"  
-wallet = Wallet(address)
+wallet = Wallet()
 
 # user/total_balance
-total_bal = wallet.get_total_balance()
+total_bal = wallet.get_total_balance(address)
 
 # user/chain_balance
 eth_chain_bal = wallet.get_chain_balance("eth")["usd_value"]
 cro_chain_bal = wallet.get_chain_balance("cro")["usd_value"]
 avax_chain_bal = wallet.get_chain_balance("avax")["usd_value"]
+
+ftm_token_balance = wallet.get_user_token_list(address, 'ftm')
